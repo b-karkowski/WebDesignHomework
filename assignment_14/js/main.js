1432fbb9edf3d63b78574e1f77cfc53ca8b05266
Rebecca Karkowski <rkarkowski079@gmail.com>
2023-12-14 22:41:59 -0700

    Update main.js

--- assignment_14/js/main.js
+++ assignment_14/js/main.js
@@ -1,10 +1,9 @@
 var canvas = document.getElementById('myCanvas');
 
-if (canvas.getContext) {
+(canvas.getContext) {
     var ctx = canvas.getContext('2d');
 
     // Title Text
     ctx.font = '80pt Arial';
     ctx.strokeText('BlacKkKlan', 100, 550);
-    ctx.textalign(center);
 }
